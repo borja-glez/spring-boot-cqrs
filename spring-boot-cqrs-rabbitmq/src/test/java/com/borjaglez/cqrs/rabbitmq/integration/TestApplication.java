232c1f7fde644df0e587ec896df2b2f5fc5b08cc
Borja Gonzalez <borja@borjaglez.com>
2026-04-03 22:48:16 +0200

    test: spring boot application test remove scanBasePackages

--- spring-boot-cqrs-rabbitmq/src/test/java/com/borjaglez/cqrs/rabbitmq/integration/TestApplication.java
+++ spring-boot-cqrs-rabbitmq/src/test/java/com/borjaglez/cqrs/rabbitmq/integration/TestApplication.java
@@ -1,10 +1,8 @@
 package com.borjaglez.cqrs.rabbitmq.integration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-import com.borjaglez.cqrs.autoconfigure.CqrsAutoConfiguration;
+@SpringBootApplication
+public class TestApplication {
 
-@SpringBootApplication(scanBasePackages = "com.borjaglez.cqrs.rabbitmq")
-@Import(CqrsAutoConfiguration.class)
-public class TestApplication {}
+}
