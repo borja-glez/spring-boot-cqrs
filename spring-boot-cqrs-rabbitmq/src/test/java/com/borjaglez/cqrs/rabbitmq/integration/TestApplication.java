67c8d78238945b886e44afc011f85454e3aa7991
Borja Gonzalez <borja@borjaglez.com>
2026-04-03 22:16:33 +0200

    test: add CqrsAutoConfiguration to test application

--- spring-boot-cqrs-rabbitmq/src/test/java/com/borjaglez/cqrs/rabbitmq/integration/TestApplication.java
+++ spring-boot-cqrs-rabbitmq/src/test/java/com/borjaglez/cqrs/rabbitmq/integration/TestApplication.java
@@ -1,6 +1,10 @@
 package com.borjaglez.cqrs.rabbitmq.integration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.borjaglez.cqrs.autoconfigure.CqrsAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "com.borjaglez.cqrs.rabbitmq")
+@Import(CqrsAutoConfiguration.class)
 public class TestApplication {}
